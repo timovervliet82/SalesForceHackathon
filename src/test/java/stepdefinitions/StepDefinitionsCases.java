2285b6f6ad926a62d45a023aa6bca1aeb7814676
Tim <timovervliet@valori.nl>
2019-01-25 14:23:54 +0100

    add step definition

--- src/test/java/stepdefinitions/StepDefinitionsCases.java
+++ src/test/java/stepdefinitions/StepDefinitionsCases.java
@@ -3,11 +3,7 @@ package stepdefinitions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.AssertionFailedError;
 import org.openqa.selenium.WebDriver;
-import pages.HomePage;
-import pages.ParticulierenPage;
-import pages.SearchModule;
 
 import static org.junit.Assert.assertTrue;
 
